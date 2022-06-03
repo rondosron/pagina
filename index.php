1c7e80645f2073cabb21160e6f4cc3b3b0c6fac2
unknown <ronanbeltrame@gmail.com>
2022-06-03 03:36:46 -0300

    Font selection + added FAQs content

--- index.php
+++ index.php
@@ -204,6 +204,7 @@
 
 			<div id="activeFilters">
 				<p>Mostrando<span id="resultFilterProduct"><span class='resultFilterProductValue'> todos los productos</span></span><span id="resultSearch"></span></p>
+				<p id="clickDetalles">(Haz click sobre un producto para más información)</p>
 			</div>
 			
 			<div id="productsContainer">
@@ -346,24 +347,24 @@
 					<div class="accordion-item">
 						<h2 class="accordion-header" id="panelsStayOpen-headingOne">
 							<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-							Accordion Item #1
+							¿La solicitud de presupuesto implica una obligación de compra?
 							</button>
 						</h2>
 						<div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
 							<div class="accordion-body">
-								<strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+								<strong>¡No!</strong> Solicitar un presupuesto es simplemente el primer paso para establecer una comunicación con la empresa. Luego, nos pondremos en contacto para ultimar los detalles del pedido (o de la consulta) y confirmar la transacción si así lo desea.
 							</div>
 						</div>
 					</div>
 					<div class="accordion-item">
 						<h2 class="accordion-header" id="panelsStayOpen-headingTwo">
 							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-							Accordion Item #2
+							Realizar una solicitud de presupuesto, ¿congela los precios?
 							</button>
 						</h2>
 						<div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
 							<div class="accordion-body">
-								<strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+								<strong>No.</strong> Los precios deberán ser confirmados por la empresa al momento de confirmar la compra.
 							</div>
 						</div>
 					</div>
